eda12b5a02df7fac993680da66578b59323ad9cc
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-07-01 23:01:10 +0600

    changed font to arial

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,29 +1,27 @@
 import "./App.css";
-import Login from "./components/LandingPage.js";
+import StartingPage from "./components/StartingPage.js";
 import Dashboard from "./components/Dashboard.js";
 import CreateTodo from "./components/CreateTodo";
 import UpdateTodo from "./components/UpdateTodo";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-      // <nav>
-      //   <ul>
-      //     <li><Link to="/">Login</Link></li>
-      //     <li><Link to="/dashboard">Dashboard</Link></li>
-      //     <li><Link to="/create-todo">CreateTodo</Link></li>
-      //     <li><Link to="/update-todo">UpdateTodo</Link></li>
-      //   </ul>
-      // </nav>
-      // <Route path="/create-todo" element={<CreateTodo />}/>
-      // <Route path="/update-todo" element={<UpdateTodo />}/>
-      function App() {
-        return (
-          <BrowserRouter>
-      <main>
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-        </Routes>
-      </main>
+// <nav>
+//   <ul>
+//     <li><Link to="/">Login</Link></li>
+//     <li><Link to="/dashboard">Dashboard</Link></li>
+//     <li><Link to="/create-todo">CreateTodo</Link></li>
+//     <li><Link to="/update-todo">UpdateTodo</Link></li>
+//   </ul>
+// </nav>
+// <Route path="/create-todo" element={<CreateTodo />}/>
+// <Route path="/update-todo" element={<UpdateTodo />}/>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<StartingPage />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
